2ae667e7c8ddbc5da61bbb657519089217a50cd4
IlTuoNome <laTuaEmail@example.com>
2025-08-28 16:33:54 +0200

    first commit

---
+++ progetto-finale-spec-frontend-back/types.ts
@@ -0,0 +1,10 @@
+export type Travel = {
+    title: string;
+    category: string;
+    description: string;
+    price: number;
+    durationDays: number;
+    img: string;
+    departure: string;
+    return: string;
+}
